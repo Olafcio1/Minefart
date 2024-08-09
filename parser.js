551eb7de441c6df901330928f640ec76a07b1e22
Olafcio <olafcio@int.pl>
2024-08-09 18:14:19 +0200

    runner 💀+downloadurl&updateurl

--- parser.js
+++ parser.js
@@ -1,17 +1,3 @@
-// ==UserScript==
-// @name         Minefart
-// @namespace    https://olafcio1.github.io
-// @version      0.1
-// @description  br0 make it b1tttter!!! Alright!
-// @author       Olafcio
-// @match        https://minefort.com/*
-// @match        http://minefort.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=minefort.com
-// @grant        GM_xmlhttpRequest
-// @run-at       document-start
-// @connect      raw.githubusercontent.com
-// ==/UserScript==
-
 (async function() {
     var src = (await new Promise(rsv => GM_xmlhttpRequest({
         method: "GET",
---
+++ runner.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Minefart
+// @namespace    https://olafcio1.github.io
+// @version      0.1
+// @description  br0 make it b1tttter!!! Alright!
+// @author       Olafcio
+// @match        https://minefort.com/*
+// @match        http://minefort.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=minefort.com
+// @grant        GM_xmlhttpRequest
+// @run-at       document-start
+// @connect      raw.githubusercontent.com
+// @downloadURL  https://raw.githubusercontent.com/Olafcio1/Minefart/main/runner.js
+// @updateURL    https://raw.githubusercontent.com/Olafcio1/Minefart/main/runner.js
+// ==/UserScript==
+
+eval("https://raw.githubusercontent.com/Olafcio1/Minefart/main/parser.js");
