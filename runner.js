b2c70cc66bacd7af59ec10cf901b8adb4aba1b7b
Olafcio <olafcio@int.pl>
2024-08-09 18:19:18 +0200

    bypass fetch rules for future

--- runner.js
+++ runner.js
@@ -14,6 +14,42 @@
 // @updateURL    https://raw.githubusercontent.com/Olafcio1/Minefart/main/runner.js
 // ==/UserScript==
 
+function parseHeaders(raw) {
+    var h = {};
+    raw = raw.split("\r\n");
+    for (let l of raw) {
+        let n = l.substring(0, l.indexOf(":"));
+        let v = l.substring(l.indexOf(":") + 2);
+        h[n] = v;
+    }
+    return h;
+}
+
+/**
+ * A web-compatible fetch function.
+ * @param {String} url The URL address to fetch.
+ * @param {Object} opts The fetch options.
+ */
+async function fetch(url, opts = {method: "GET"}) {
+    if (typeof opts.body !== 'undefined')
+        opts.data = opts.body;
+    let r = await GM.xmlHttpRequest({
+        url: url,
+        ...opts
+    });
+    let headers = parseHeaders(r.responseHeaders);
+    return {
+        text: () => r.responseText,
+        json: () => JSON.parse(r.responseText),
+        blob: () => URL.createObjectURL(new Blob([r.responseText], {type: headers["content-type"]})),
+//        xml: () => r.responseXML,
+        status: r.status,
+        statusText: r.statusText,
+        url: r.finalUrl,
+        headers: headers
+    };
+}
+
 fetch("https://raw.githubusercontent.com/Olafcio1/Minefart/main/parser.js").then(async r => {
     eval(await r.text());
 });
